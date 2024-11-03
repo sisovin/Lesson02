460e927aa86ef37bd7c03d94861c381cce8708cb
Sisovin Chieng <sisovin@gmail.com>
2024-11-03 20:05:58 +0700

    Initial commit

---
+++ testexample.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 19 09:18:36 2024
+
+@author: chien
+"""
+
+import torch
+
+x = torch.tensor([1, 2, 3])
+print(x)
+
+x = torch.rand(2, 3)
+print(x)
+
+
+
